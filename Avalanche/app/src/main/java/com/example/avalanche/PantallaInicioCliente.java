0bda4383722547605b25d614127e0fcf53351675
Roxep5 <pinhayop@gmail.com>
2021-05-31 16:44:42 +0200

    fragmentos y viewpager

--- Avalanche/app/src/main/java/com/example/avalanche/PantallaInicioCliente.java
+++ Avalanche/app/src/main/java/com/example/avalanche/PantallaInicioCliente.java
@@ -4,20 +4,45 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class PantallaInicioCliente extends AppCompatActivity {
 
     public static final int CODIGO=1;
+    private TabLayout tlCliente;
+    ViewPager viewPager;
+
+    private SectionsPagerAdapter sectionsPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicio_cliente);
+
+        tlCliente=findViewById(R.id.tlCliente);
+
+
+        sectionsPagerAdapter=new SectionsPagerAdapter(getSupportFragmentManager());
+
+
+        viewPager=findViewById(R.id.viewpager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        //TabLayout.OnTabSelectedListener
+
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tlCliente));
+        tlCliente.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -74,6 +99,56 @@ public class PantallaInicioCliente extends AppCompatActivity {
                     finish();
                 }
             }
+        }
+    }
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+    }
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            Fragment fragment =null;
+            switch (position) {
+                case 0:
+                    fragment = new BlankFragment();
+                    break;
+                case 1:
+                    fragment = new BlankFragment2();
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
         }
     }
 }
